65bed0ceb3f2604dd067d7c186115f9f7ef388d7
wolfb25 <fbalint0126@gmail.com>
2024-04-23 13:09:09 +0200

    Revert "kesz"
    This reverts commit d136a4fa8ac99975258ebcf068895bf4a11feace.

--- MauiApp2/MainPage.xaml.cs
+++ MauiApp2/MainPage.xaml.cs
@@ -1,43 +1,21 @@
-﻿using System;
-using System.IO;
-using System.Text;
-using System.Collections.Generic;
-using Microsoft.Maui.Controls;
-using System.Collections.ObjectModel;
+﻿namespace MauiApp2 {
+	public partial class MainPage : ContentPage {
+		int count = 0;
 
-namespace Utasszallitok2
-{
-    public partial class MainPage : ContentPage
-    {
-        ObservableCollection<string> results = new ObservableCollection<string>();
-        public MainPage()
-        {
-            InitializeComponent();
-            resultList.ItemsSource = results;
-        }
+		public MainPage() {
+			InitializeComponent();
+		}
 
+		private void OnCounterClicked(object sender, EventArgs e) {
+			count++;
 
-        private void CalculateMachNumber(object sender, EventArgs e)
-        {
-            try
-            {
-                double q = double.Parse(torlo.Text);
-                double p = double.Parse(statik.Text);
-                double m = Math.Sqrt(5 * (Math.Pow(q / p + 1, 2.0 / 7) - 1));
-                if (m < 1)
-                {
-                    results.Add($"qc={q} p0={p} Ma={m}");
-                }
-                else
-                {
-                    DisplayAlert("Hiba", "A Mach-szám értéke nagyobb vagy egyenlő mint 1.", "OK");
-                }
-            }
-            catch (Exception ex)
-            {
-                DisplayAlert("Hiba", ex.Message, "OK");
-            }
-        }
+			if (count == 1)
+				CounterBtn.Text = $"Clicked {count} time";
+			else
+				CounterBtn.Text = $"Clicked {count} times";
 
-    }
+			SemanticScreenReader.Announce(CounterBtn.Text);
+		}
+	}
+
 }
